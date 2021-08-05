10414bd0a4f165f44be2ab89da31213746c8e5db
Sebastian <mocart15@gmail.com>
2021-08-05 16:06:31 +0200

    Created installable package.

---
+++ efficientnet_v2/__init__.py
@@ -0,0 +1,21 @@
+"""Package with exposed EfficientNet V2 models."""
+
+from efficientnet_v2.efficientnet_v2 import (
+    EfficientNetV2B0,
+    EfficientNetV2B1,
+    EfficientNetV2B2,
+    EfficientNetV2B3,
+    EfficientNetV2L,
+    EfficientNetV2M,
+    EfficientNetV2S,
+)
+
+__all__ = [
+    "EfficientNetV2B0",
+    "EfficientNetV2B1",
+    "EfficientNetV2B2",
+    "EfficientNetV2B3",
+    "EfficientNetV2S",
+    "EfficientNetV2M",
+    "EfficientNetV2L",
+]
