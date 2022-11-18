bb439d42eb7f51bab658ec0b43e5f30a4e16da2e
Sebastian <mocart15@gmail.com>
2022-11-18 15:09:17 +0100

    Simplified package due to dependency conflicts.

--- efficientnet_v2/efficientnet_v2.py
+++ efficientnet_v2/efficientnet_v2.py
@@ -9,13 +9,19 @@ from absl import logging
 from packaging import version
 from tensorflow.python.keras import backend
 
-# Keras has been moved to separate repository
+# Keras has been moved to separate repository in 2.9
 if version.parse(tf.__version__) < version.parse("2.8"):
     from tensorflow.python.keras.applications import imagenet_utils
 else:
     from keras.applications import imagenet_utils
-from tensorflow.python.keras.utils import layer_utils
-from tensorflow.python.lib.io import file_io
+
+# tensorflow.python.keras is removed in 2.12
+if version.parse(tf.__version__) < version.parse("2.12"):
+    from tensorflow.python.keras import backend
+    from tensorflow.python.keras.utils import layer_utils
+else:
+    from tensorflow.keras import backend
+    from keras.utils import layer_utils
 
 from efficientnet_v2.blocks_args import BLOCKS_ARGS
 
